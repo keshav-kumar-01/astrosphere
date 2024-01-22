ce10588f0d8933f0b26be3411d5524c31b005d1a
keshav kumar <76901651+keshav-kumar-01@users.noreply.github.com>
2024-01-22 16:20:28 +0530

    initial commit

---
+++ space/quiz/index.3ac99f0c.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Which planet is known as Red Planet ?",
+        answer: "Mars",
+        options: [
+            "Venus",
+            "Mars",
+            "Jupiter",
+            "Saturn"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which planet is the closest to the Sun??",
+        answer: "Mercury",
+        options: [
+            "Mercury",
+            "Venus",
+            "Mars",
+            "Earth"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which planet is the largest in our solar system?",
+        answer: "Jupiter",
+        options: [
+            "Earth",
+            "Neptune",
+            "Jupiter",
+            "Uranus"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which planet is known for its beautiful ring system?",
+        answer: "Saturn",
+        options: [
+            "Saturn",
+            "Uranus",
+            "Jupiter",
+            "Neptune"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which planet is often called the Evening Star or Morning Star?",
+        answer: "Venus",
+        options: [
+            "Venus",
+            "Mars",
+            "Mercury",
+            "Jupiter"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which planet has the most significant tilt in its rotational axis, giving it extreme seasons?",
+        answer: "Uranus",
+        options: [
+            "Uranus",
+            "Mars",
+            "Saturn",
+            "Jupiter"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which planet is known for its stunning blue color due to methane in its atmosphere?",
+        answer: "Neptune",
+        options: [
+            "Uranus",
+            "Jupiter",
+            "Neptune",
+            "Saturn"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which planet has the Great Red Spot, a giant storm system?",
+        answer: "Jupiter",
+        options: [
+            "Jupiter",
+            "Saturn",
+            "Mars",
+            "Earth"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which planet is known as the Ice Giant ?",
+        answer: "Neptune",
+        options: [
+            "Uranus",
+            "Neptune",
+            "Saturn",
+            "Jupiter"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which planet is often referred to as the Twin of Earth due to its similarities in size and density?",
+        answer: "Venus",
+        options: [
+            "Venus",
+            "Mars",
+            "Mercury",
+            "Uranus"
+        ]
+    }
+];
+
+//# sourceMappingURL=index.3ac99f0c.js.map
